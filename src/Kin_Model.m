10b33d54d4ebf485fcd82c60c5cf57694f5e42c0
Nick Panzarino <panzarino23@gmail.com>
2016-03-02 19:26:30 -0500

    Inverse Jacobian and lambda

--- src/Kin_Model.m
+++ src/Kin_Model.m
@@ -1,38 +1,63 @@
 classdef Kin_Model
-    %Kin_Model Model of a serial robot manipulator
+    %Kin_Model: Kinematic Model of a serial robot manipulator
     
     properties
+    
+    end
+    
+    properties(SetAccess = private)
         q
         DOF
         J
         inv_J
     end
     
-    properties(Access = public)
+    properties(Access = private)
         T_matrices
+        lambda_val = 0  %small positive real value used to remove singularities in inverse Jacobian
+        lambda_inv_J    %inverse Jacobian with symbolic lambda to quickly recalculate when lambda changes
+        sym_lambda = sym('lambda');
     end
     
     properties (Dependent)
-        
+        lambda
     end
     
     methods
-        % Constructor. Takes in Chain of Homogeneous Transformation
-        % Matrices
-        
+
         function value = T(obj,from,to)
             value = obj.T_matrices{from+1,to+1};
         end 
+        
+        function value = forward_kin(obj,q)
+            value = subs(obj.T(0,size(obj.DOF)),obj.q,q);
+        end
+        
+        function value = inv_kin(obj,H)
+            error('function not implemented')
+        end
+        
+        function obj = set.lambda(obj,value)
+            obj.lambda_val = value;
+            if obj.lambda_inv_J ~= double([])
+                obj.inv_J = simplify(vpa(subs(obj.lambda_inv_J,obj.sym_lambda,obj.lambda)));
+            end
+        end
+        function value = get.lambda(obj)
+            value = obj.lambda_val;
+        end
     end
     
     methods(Static)
         function obj = fromH_TransChain(varargin)
             obj = Kin_Model;
-            obj = obj.init(size(DH_Matrix,1));
+            obj = obj.init(size(varargin{1},1));
             for i = 1:obj.DOF
                 obj.T_matrices{i,i+1}=vpa(varargin{i});
             end
             obj=obj.calculateFromChain();
+            obj=obj.calculateJacobian();
+            obj=obj.calculatePesudoInvJacobian();
         end
         
         function obj=fromDH(DH_Matrix)
@@ -42,11 +67,13 @@ classdef Kin_Model
                 obj.T_matrices{i,i+1}=simplify(vpa(H_Trans.fromDH(DH_Matrix(i,:)).H));
             end
             obj=obj.calculateFromChain();
+            obj=obj.calculateJacobian();
+            obj=obj.calculatePesudoInvJacobian();
         end
         
     end
     
-    methods(Access = public)
+    methods(Access = private)
         function obj = init(obj,size)
             obj.DOF=size;
             obj.q=sym('q',[1,obj.DOF]);
@@ -78,22 +105,17 @@ classdef Kin_Model
             end
         end
         
-        function obj = calculateFromAbsolute(obj)
-            for i=2:size(obj.T_matrices,2)
-                obj.T_matrices{i,1} = simplify(vpa(inv(obj.T_matrices{1,i})));
-            end
-            
-            for r = 2:size(obj.T_matrices,1)
-               for c = 2:size(obj.T_matrices,2)
-                   if c~=r
-                       obj.T_matrices{r,c} = simplify(vpa(obj.T_matrices{r,1}*obj.T_matrices{1,c}));
-                   end
-               end
-            end
+        function obj = calculateJacobian(obj)
+            obj.J=H_Trans(obj.T(0,size(obj.q))).getJacobian(obj.q);
         end
         
-        function obj = calculateJacobian(obj)
-            obj.J=H_Trans(obj.T(0,obj.DOF)).getJacobian(obj.q);
+        function obj = calculatePesudoInvJacobian(obj,lambda)
+            if nargin<2
+                lambda = obj.lambda;
+            end
+            
+            obj.lambda_inv_J = simplify(vpa(obj.J.'/(obj.J*obj.J.' + (obj.sym_lambda^2).*eye(max(size(J))))));
+            obj.lambda = lambda;
         end
     end
     
